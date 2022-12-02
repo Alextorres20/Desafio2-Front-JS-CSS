1ef7338d6c1c246db38e23823c3676c207045fd1
Alextorres20 <alex56789torres@gmail.com>
2022-12-02 22:16:03 +0100

    Creada Pagina Humanos, Falta diseñarla y funcional

---
+++ src/js/humanos/index.js
@@ -0,0 +1,13 @@
+// Import our custom CSS
+import '../../styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// Imports JavaScript
+import { initModal } from './modal-matar-humanos';
+
+initModal();
+
+
+
