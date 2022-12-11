96497e6bf6c3a75696c66f670d1a1d6465ccf2b9
aliciafdezc <cfdezalicia@gmail.com>
2022-12-11 14:40:20 +0100

    - Cargar humanos html

--- src/js/asignar-pruebas.js
+++ src/js/asignar-pruebas.js
@@ -1,2 +1,50 @@
 import '../styles.scss';
+import { obtenerHumanosDios, mostrarCaracteristicasHumano } from './humanos/http-proveedores';
 
+
+const initAsignarPruebas = () => {
+    cargarHumanos();
+    cargarPruebas();
+}
+
+
+const cargarHumanos = async() => {
+    const humanos = await obtenerHumanosDios();
+    humanos.map(generarHumanoHtml);
+}
+
+
+const generarHumanoHtml = async(humano) => {
+    const atributos = await mostrarCaracteristicasHumano(humano.id_usuario);
+    const html = `<div class="humano mb-2">
+                    <img class="imagen-perfil" src="../assets/img/Zeus.png">
+                    <div class="datos">
+                        <h5 class="nombre fw-bold">${[humano.user.name]}</h5>
+                        <p class="destino"><span class="fw-bold">Destino: </span>${[humano.destino]}</p>
+                    </div>
+                    <div class="atributos">${[generarAtributosHtml(atributos)]}</div>
+                    <div class="borrar"></div>
+                </div>`;
+
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    document.querySelector('.contenedor.humanos .card-body').append(div.firstElementChild);
+}
+
+
+const generarAtributosHtml = (atributos) => { 
+    let html = '';
+    atributos.forEach(a => {
+        let nombre = a.caracteristica.nombre;
+        html += `<p><span class="fw-bold">${[ (nombre.charAt(0).toUpperCase() + nombre.slice(1)).replaceAll('-', ' ')]}:</span> ${[a.valor]}</p>`;
+    });
+
+    return html;
+}
+
+const cargarPruebas = () => {
+
+}
+
+
+initAsignarPruebas();
--- src/js/humanos/http-proveedores.js
+++ src/js/humanos/http-proveedores.js
@@ -1,10 +1,14 @@
-import { recuperarToken } from '../auxiliar/local-storage';
+import { token } from "../..";
 
-const urlMostrarUnHumanoVivo = 'http://127.0.0.1:8000/api/mostrarHumanoVivo';
-const urlMostrarHumanosVivos = 'http://127.0.0.1:8000/api/mostrarHumanosVivos';
-const urlCrearHumanoscomoDios = 'http://127.0.0.1:8000/api/crearUsuarios';
-const urlMostrarCaracteristicas_Dios = 'http://127.0.0.1:8000/api/mostrarCaracteristicas_Dios';
-const token = recuperarToken();
+const urlMostrarUnHumanoVivo = 'http://127.0.0.1:8000/api/mostrarHumanoVivo',
+    urlMostrarHumanosVivos = 'http://127.0.0.1:8000/api/mostrarHumanosVivos',
+    urlCrearHumanoscomoDios = 'http://127.0.0.1:8000/api/crearUsuarios',
+    urlMostrarCaracteristicas_Dios = 'http://127.0.0.1:8000/api/mostrarCaracteristicas_Dios',
+    urlMostrarCaracteristicasHumano = 'http://127.0.0.1:8000/api/mostrarCaracteristicasHumano',
+    urlObtenerHumanosDios = 'http://127.0.0.1:8000/api/obtenerHumanosDios'; 
+
+
+//Alejandro
 const MostrarUnHumanoVivo = async(id) => {
     const resp = await fetch(`${urlMostrarUnHumanoVivo}/${id}`, {
         method: 'GET',
@@ -16,15 +20,15 @@ const MostrarUnHumanoVivo = async(id) => {
     const data = await resp.json();
     return data;
 }
+
+//Alejandro
 const mostrarUsuariosVivos = async() => {
-    
-    console.log(token);
     const resp = await fetch(urlMostrarHumanosVivos, {
         method: 'GET',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization' : `Bearer ${token}`
+            'Authorization' : 'Bearer ' + token
             
         }
     });
@@ -32,10 +36,8 @@ const mostrarUsuariosVivos = async() => {
     return data;
 }
 
+//Alejandro
 const crearHumanos = async(cantidad)=> {
-    const token = recuperarToken();
-    console.log(JSON.stringify(cantidad));
-    console.log(cantidad);
     const resp = await fetch(urlCrearHumanoscomoDios, {
         method: 'POST',
         body: JSON.stringify(cantidad),
@@ -48,15 +50,42 @@ const crearHumanos = async(cantidad)=> {
     return data;
 }
 
+//Alejandro
 const mostrarCaracteristicas_Dios = async(id_usuario)=> {
     const resp = await fetch(`${urlMostrarCaracteristicas_Dios}/${id_usuario}`);
     const data = await resp.json();
     return data;
 }
 
+//Alicia
+const mostrarCaracteristicasHumano = async(id_usuario)=> {
+    const resp = await fetch(`${urlMostrarCaracteristicasHumano}/${id_usuario}`);
+    const { caracteristicas } = await resp.json();
+    return caracteristicas;
+}
+
+//Alicia
+const obtenerHumanosDios = async() => {
+    try {
+        const data = await fetch(urlObtenerHumanosDios, {
+            method: 'GET',
+            headers: {'Authorization': 'Bearer ' + token }
+        });
+        if (!data.ok) throw Error('Error');
+        const respuesta =  await data.json();
+        console.log(respuesta);
+        return respuesta;
+    } catch (err) {
+        throw Error('No se han encontrado resultados');
+    }
+}
+
+
 export {
     MostrarUnHumanoVivo,
     mostrarUsuariosVivos,
     crearHumanos,
-    mostrarCaracteristicas_Dios
+    mostrarCaracteristicas_Dios,
+    mostrarCaracteristicasHumano,
+    obtenerHumanosDios
 }
